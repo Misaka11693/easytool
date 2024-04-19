9a8a56f76494006f21faea2d9851f7f456893f94
Misaka11639 <1647988592@qq.com>
2024-04-19 20:03:49 +0800

    【单例工具类】对恶汉模式的调证

--- EasyTool.Core/LanguageCategory/SingletonUtil.cs
+++ EasyTool.Core/LanguageCategory/SingletonUtil.cs
@@ -9,38 +9,48 @@ namespace EasyTool
     /// </summary>
     public class SingletonUtil<T> where T : class, new()
     {
+        // 懒汉模式
         private static readonly Lazy<T> lazyInstance = new Lazy<T>(() => new T());
 
+        // 饿汉模式
+        private static readonly T eagerInstance = new T();
+
         /// <summary>
-        /// 返回单例对象的唯一实例（懒汉模式）
+        /// 返回懒汉模式的单例对象
         /// </summary>
         public static T LazyInstance => lazyInstance.Value;
 
-
-        private static T instance;
-        private static readonly object lockObject = new object();
-
         /// <summary>
-        /// 返回单例对象的唯一实例（饿汉模式）
+        /// 返回饿汉模式的单例对象
         /// </summary>
-        [Obsolete]
-        public static T Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    lock (lockObject)
-                    {
-                        if (instance == null)
-                        {
-                            instance = new T();
-                        }
-                    }
-                }
+        public static T EagerInstance => eagerInstance;
 
-                return instance;
-            }
-        }
+        #region 饿汉模式(Obsolete)
+        //    private static T instance;
+        //    private static readonly object lockObject = new object();
+
+        //    /// <summary>
+        //    /// 返回单例对象的唯一实例（饿汉模式）
+        //    /// </summary>
+        //    [Obsolete]
+        //    public static T Instance
+        //    {
+        //        get
+        //        {
+        //            if (instance == null)
+        //            {
+        //                lock (lockObject)
+        //                {
+        //                    if (instance == null)
+        //                    {
+        //                        instance = new T();
+        //                    }
+        //                }
+        //            }
+
+        //            return instance;
+        //        }
+        //    } 
+        #endregion
     }
 }
