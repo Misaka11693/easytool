5a29df1524dc7682fa6a929ab4edbb61c8030e77
Misaka11639 <1647988592@qq.com>
2024-04-23 09:54:04 +0800

    新增【ObjectExtensions】工具栏，用来编写对象的扩展方法

---
+++ EasyTool.Core/ToolCategory/ObjectExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+public static class ObjectExtensions
+{
+    /// <summary>
+    /// 检查对象不为空（不为 null 且不为空字符串或空白字符）
+    /// </summary>
+    public static bool IsNotEmpty(this object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+
+        if (obj is string str)
+        {
+            return !string.IsNullOrWhiteSpace(str);
+        }
+
+        if (obj is ICollection collection)
+        {
+            return collection.Count == 0;
+        }
+
+        return true;
+    }
+}
